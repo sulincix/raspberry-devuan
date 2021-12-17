05ed6c9f1f4186c395fc409123fdc74fc5906740
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2021-12-17 19:50:56 +0000

    Update build.sh

---
+++ build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -ex
+# create boot
+mkdir -p work
+cd work
+# fetch device firmware (prebuilt)
+[[ -f firmware.zip ]] || wget -c https://github.com/raspberrypi/firmware/archive/refs/heads/master.zip -O firmware.zip
+yes "A" | unzip firmware.zip
+cd ..
+mkdir -p rootfs/boot
+cp -rvf work/firmware-master/boot/* rootfs/boot/
+cat > rootfs/boot/cmdline.txt << EOF
+console=ttyS1,115200 console=tty0 root=LABEL=ROOTFS rw net.ifnames=0 rootwait
+EOF
+cat > rootfs/boot/cmdline.txt << EOF
+# Switch the CPU from ARMv7 into ARMv8 (aarch64) mode
+arm_64bit=1
+
+enable_uart=1
+upstream_kernel=1
+
+## memory shared with the GPU
+gpu_mem=128
+
+## always audio
+dtparam=audio=on
+
+## maximum amps on usb ports
+max_usb_current=1
+
+## enable hardware-accelerated graphics
+dtoverlay=vc4-kms-v3d
+
+## kernel
+kernel=kernel8.img
+EOF
+# install required packages if host debian
+if [[ -d /var/lib/apt/ ]] ; then
+    apt install qemu-user-static binfmt-support debootstrap -y
+fi
+debootstrap --foreign --no-check-gpg --no-merged-usr --arch=arm64 stable work/rootfs https://pkgmaster.devuan.org/merged
+
+cp $(which qemu-aarch64-static) work/rootfs/usr/bin/qemu-aarch64-static
+if which service ; then
+    service binfmt-support start
+fi
+echo "nameserver 1.1.1.1" > work/rootfs/etc/resolv.conf
+echo "APT::Sandbox::User root;" > work/rootfs/etc/apt/apt.conf.d/99sandboxroot
+#### Configure system
+cat > work/rootfs/etc/apt/apt.conf.d/01norecommend << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
+cp -rvf work/firmware-master/modules/* rootfs/lib/modules/
+echo "deb http://pkgmaster.devuan.org/devuan merged main contrib non-free" > work/rootfs/etc/apt/sources.list
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/bash -c "apt-get update"
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/bash -c "apt-get install network-manager openssh-server -y"
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/bash -c "apt-get install firmware-linux raspi-firmware -y"
+cp work/
+for i in $(ls rootfs/lib/modules) ; do
+    chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/bash -c "depmod -a $i"
+done
+cat work/rootfs/vmlinuz > rootfs/boot/vmlinuz
+cat work/rootfs/initrd.img > rootfs/boot/initrd.img
+size=$(du -s "work/rootfs" | cut -f 1)
+qemu-img create "devuan.img" $(($size*1500+300*1024*1024))
+parted "devuan.img" mklabel msdos
+echo Ignore | parted "devuan.img" mkpart primary fat32 0 300M 
+echo Ignore | parted "devuan.img" mkpart primary ext2 301M 100%
+losetup -d /dev/loop0
+loop=$(losetup --partscan --find --show "devuan.img" | grep "/dev/loop")
+mkfs.vfat ${loop}p1
+yes | mkfs.ext4 ${loop}p2 -L "ROOTFS"
+mount ${loop}p1 /mnt
+cp -prfv rootfs/boot/* /mnt/
+sync
+umount -f /mnt
+mount ${loop}p2 /mnt
+cp -prfv work/rootfs/* /mnt/
+sync
+umount /mnt
